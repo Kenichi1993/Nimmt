489bbcba52c1c0d42508501c6e67c2fef7a6595a
Kenichi1993 <kfujita351@gmail.com>
2024-01-18 01:42:00 +0900

    feat: class template of players

---
+++ players/Player.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+struct PLAYER{
+    int turn;
+    vector<vector<int>> field;
+    vector<int> score;
+
+    vector<int> hand;
+
+    virtual int select_card(){
+        //function for select card
+        return 0;
+    }
+    virtual int select_lane(){
+        //function for select lane
+        return 0;
+    }
+};
